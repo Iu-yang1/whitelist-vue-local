b2eb166cb501df0b4fb9111e60105fcd6cc7e2d7
Angicono <2758233067@qq.com>
2025-04-23 10:38:40 +0800

    feature:白名单注册引导

---
+++ src/config/links.js
@@ -0,0 +1,15 @@
+export const QQ_GROUP = 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=AzNmCjtD9hT9b2QPHxFg1_yJlhMGVPZa&authKey=G3Yy0lqOyWo2eoWk8J%2F47eLNBYsHVToqdAsUxywAfAhiW8pf8OYwItin4cdlbEm5&noverify=0&group_code=794710920'
+
+export const DISCORD = ''
+
+export const BILIBILI = 'https://www.bilibili.com/video/BV1UL9MYVE1V/?spm_id_from=333.1387.homepage.video_card.click&vd_source=896b941b0e95fbb244fb5746bc33f518'
+
+export const SERVER_STATUS = 'https://status.impart-lab.tech/'
+
+export const WIKI = 'https://wiki.impart-lab.tech/'
+
+export const WORLD_MAP = 'https://map-s.impart-lab.tech/'
+
+export const JOIN_US = 'https://www.bilibili.com/video/BV1GJ411x7h7?buvid=Y7450474DB0CC7AA404983FDF9579B11464F&from_spmid=search.search-result.0.0&is_story_h5=false&mid=jZaKa2hzg4cPM4WXsNhDNw%3D%3D&plat_id=116&share_from=ugc&share_medium=iphone&share_plat=ios&share_session_id=5604DA38-7D0B-4A72-AE38-B8084E2ECE26&share_source=QQ&share_tag=s_i&spmid=united.player-video-detail.0.0&timestamp=1741323278&unique_k=2WhKHcc&up_id=486906719&vd_source=6f8027fdc34aad1b09ab42e0bc9c8a0a'
+
+export const WHITELIST = 'https://wl.impart-lab.tech/'
