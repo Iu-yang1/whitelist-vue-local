3f01e2ce4872df1de1bef5b670e12ebb067f26d4
Angicono <2758233067@qq.com>
2025-05-19 10:32:55 +0800

    fix:第三步引导修改

--- src/config/links.js
+++ src/config/links.js
@@ -13,3 +13,5 @@ export const WORLD_MAP = 'https://map-s.impart-lab.tech/'
 export const JOIN_US = 'https://www.bilibili.com/video/BV1GJ411x7h7?buvid=Y7450474DB0CC7AA404983FDF9579B11464F&from_spmid=search.search-result.0.0&is_story_h5=false&mid=jZaKa2hzg4cPM4WXsNhDNw%3D%3D&plat_id=116&share_from=ugc&share_medium=iphone&share_plat=ios&share_session_id=5604DA38-7D0B-4A72-AE38-B8084E2ECE26&share_source=QQ&share_tag=s_i&spmid=united.player-video-detail.0.0&timestamp=1741323278&unique_k=2WhKHcc&up_id=486906719&vd_source=6f8027fdc34aad1b09ab42e0bc9c8a0a'
 
 export const WHITELIST = 'https://wl.impart-lab.tech/'
+
+export const QQ_MAIL = 'https://mail.qq.com/'
